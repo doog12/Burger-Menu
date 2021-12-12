da9a4b7b80a3682b160a452edf4594eb7b51904f
doog12 <drannikov.kirill@gmail.com>
2021-12-12 16:59:18 +0200

    Bug Fix & Code Revision

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,19 @@
 // Developed by DOOG12
 
-let burgerMenu = document.querySelector('.burger-menu')
-let burgerNav = document.querySelector('.burger-nav')
+window.addEventListener('DOMContentLoaded', () => {
+  const burgerMenu = document.querySelector('.burger-menu')
+  const burgerNav = document.querySelector('.burger-nav')
+  const burgerNavLink = document.querySelectorAll('.burger-nav__link')
 
-burgerMenu.addEventListener('click', function() {
-   burgerMenu.classList.toggle('active')
-   burgerNav.classList.toggle('active')
+  burgerMenu.addEventListener('click', function () {
+    burgerMenu.classList.toggle('active')
+    burgerNav.classList.toggle('active')
+  })
+
+  for (let i = 0; i < burgerNavLink.length; i++) {
+    burgerNavLink[i].addEventListener('click', () => {
+      burgerMenu.classList.toggle('active')
+      burgerNav.classList.toggle('active')
+    })
+  }
 })
